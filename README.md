29b0dc69d0726ead6dbbc9f2c0a8e27126880fcb
franck.ledoux <franck.ledoux@cea.fr>
2023-07-03 10:03:17 +0200

    Add 2 python tests (mesh and step)

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # mgx
 
 ![CI Ubuntu](https://github.com//LIHPC-Computational-Geometry/mgx/actions/workflows/ci-ubuntu.yml/badge.svg)
-Software for 3D geometry preparation and mesh generation.
+
+Mgx, is an interactive software dedicated to block-structured hex meshing. It provides many interactive tools to design
+blocks. It also give access to basic functionalities to build CAD models.
--- test_link/test_mesh.py
+++ test_link/test_mesh.py
@@ -6,9 +6,13 @@ def test_topo_box():
     gm = ctx.getGeomManager ()
     tm = ctx.getTopoManager ()
     mm = ctx.getMeshManager()
+    #create a single box with one block
     tm.newBoxWithTopo (Mgx3D.Point(0, 0, 0), Mgx3D.Point(1, 1, 1), 10, 10, 10)
-    assert tm.getNbBlocks()==1
-    assert tm.getNbFaces()==6
 
+    # mesh blocks now
+    mm.newAllBlocksMesh()
+    assert mm.getNbNodes()==1331
+    assert mm.getNbFaces()==600
+    assert mm.getNbRegions()==1000
     #This last command is mandatory to clean the session for the next test
     ctx.clearSession()
